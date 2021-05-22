a122db5555f097ed2408dc334458a3183c30d460
elixter <ltw971@naver.com>
2021-05-22 15:45:32 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import utils
+import os
+import time
+import boto3
+
+
+def clustering():
+    dataserver = os.environ['DATASERVER']
+    news_df = utils.get_data_from_api(dataserver)
+    ftr_vect = utils.generate_tfidf(news_df)
+    utils.cluster_to_csv(ftr_vect, news_df)
+
+    current_time = time.strftime('%Y%m%d')
+    csvfile = f'./{current_time}.csv'
+    news_df.to_csv(csvfile, sep=',', encoding='utf-8', index=None)
+
+    s3 = boto3.client('s3')
+    bucket_name = 'newsclusterstorage'
+
+    try:
+        s3.upload_file(csvfile, bucket_name, csvfile)
+    except:
+        print(f'Upload to s3 bucket is failed.')
+    else:
+        print(f'Result of clustering at {current_time} is uploaded')
+
---
+++ utils.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+import hdbscan
+from konlpy.tag import Okt
+import requests
+
+
+def get_data_from_api(url):
+    try:
+        res = requests.get(url)
+    except requests.exceptions.ConnectionError as e:
+        print(e)
+        return None
+    except requests.exceptions.RequestException as e:
+        print(e)
+        return None
+
+    json_data = res.json()
+
+    return pd.DataFrame(json_data)
+
+
+def generate_tfidf(df, min_df=0.01, max_df=0.8):
+    okt = Okt()
+    print('Generate tf-idf vector...')
+    tfidf_vect = TfidfVectorizer(tokenizer=okt.morphs,
+                                 ngram_range=(1, 2),
+                                 min_df=min_df, max_df=max_df)
+
+    ftr_vect = tfidf_vect.fit_transform(df['body'])
+    print('tf-idf vector is generated.')
+
+    return ftr_vect
+
+
+def cluster_to_csv(vect, df, min_cluster_size=12):
+    print('Start clustering....')
+    cluster = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size, gen_min_span_tree=True)
+    res = cluster.fit(vect)
+    df['label'] = res.labels_
+    print('Clustering is finished.')
